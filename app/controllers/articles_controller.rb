fac9a0ab91c6f131858b995a252c7b72640821c1
lpokrityuk <lpokrityuk@fleetsmart.co.uk>
2017-09-17 15:27:54 +0100

    commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,10 +12,21 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
 
-    redirect_to articles_show(@article)
+      redirect_to article_path(@article)
+      flash[:notice] = "Article was successfully created"
+    else
 
+      render 'new'
+    end
+
+
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
 
